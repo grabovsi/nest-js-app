2059119751cba8eab7e2c7669556986605a9c4fb
Ivan Grabovski <ivangrabovski@gmail.com>
2022-01-24 12:17:55 +0300

    test2

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  // test comment 222
+  // test comment 222333333333333333
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
